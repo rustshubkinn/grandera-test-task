fcd69d7fb40cf99f11e38b17476481bc9dac69fc
Rustam Shubkin <rustshubkinn@mail.ru>
2021-10-22 22:19:06 +0600

    Add Routes for Home and User pages in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import { Home } from './pages/Home/Home';
+import { Switch, Route } from 'react-router-dom';
+
+import { Home } from 'pages/Home/Home';
+import { User } from 'pages/User/User';
 
 import './global.scss';
 
-export const App = () => <Home />;
+export const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/user/:id" component={User} />
+  </Switch>
+);
